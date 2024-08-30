5319dff4a102331078f30ba05093277e1e060b53
Vijay Saravana Jaishanker <vjaishanker3@gatech.edu>
2024-08-30 12:33:30 -0700

    fix bug

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,8 +1,5 @@
-import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
-import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
